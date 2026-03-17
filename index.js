48da74e656d77ffa9c8d35cc33fe12976fcfb932
summeet95 <summeet95@gmail.com>
2026-03-17 11:11:48 +0000

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  return res.json({ msg: 'Hello from server!' });
+  return res.json({ msg: 'Hello from server! this is second changes' });
 });
 
 app.listen(PORT, () => {
